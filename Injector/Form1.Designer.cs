981bb1a4707cf6f990daf8054dca07b43b2ff8c3
Rigqx <79773832+Rigqx@users.noreply.github.com>
2023-09-10 12:59:28 +0200

    Add files via upload

---
+++ Injector/Form1.Designer.cs
@@ -0,0 +1,241 @@
+﻿namespace Injector
+{
+    partial class Injector
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Panel = new Siticone.Desktop.UI.WinForms.SiticonePanel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.siticoneTextBox1 = new Siticone.Desktop.UI.WinForms.SiticoneTextBox();
+            this.button = new Siticone.Desktop.UI.WinForms.SiticoneRoundedButton();
+            this.siticoneRoundedComboBox1 = new Siticone.Desktop.UI.WinForms.SiticoneRoundedComboBox();
+            this.button2 = new Siticone.Desktop.UI.WinForms.SiticoneRoundedButton();
+            this.siticoneRoundedButton1 = new Siticone.Desktop.UI.WinForms.SiticoneRoundedButton();
+            this.label3 = new System.Windows.Forms.Label();
+            this.Panel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Panel
+            // 
+            this.Panel.AutoRoundedCorners = true;
+            this.Panel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(20)))), ((int)(((byte)(20)))));
+            this.Panel.BorderRadius = 20;
+            this.Panel.Controls.Add(this.label2);
+            this.Panel.Controls.Add(this.label1);
+            this.Panel.Location = new System.Drawing.Point(-10, -3);
+            this.Panel.Name = "Panel";
+            this.Panel.ShadowDecoration.Parent = this.Panel;
+            this.Panel.Size = new System.Drawing.Size(367, 42);
+            this.Panel.TabIndex = 0;
+            this.Panel.Paint += new System.Windows.Forms.PaintEventHandler(this.Panel_Paint);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.label2.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(319, 12);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(23, 22);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "X";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(23, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(90, 18);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "DLL Injector";
+            // 
+            // siticoneTextBox1
+            // 
+            this.siticoneTextBox1.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.siticoneTextBox1.DefaultText = "";
+            this.siticoneTextBox1.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.siticoneTextBox1.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.siticoneTextBox1.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.siticoneTextBox1.DisabledState.Parent = this.siticoneTextBox1;
+            this.siticoneTextBox1.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.siticoneTextBox1.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(50)))), ((int)(((byte)(50)))));
+            this.siticoneTextBox1.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.siticoneTextBox1.FocusedState.Parent = this.siticoneTextBox1;
+            this.siticoneTextBox1.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.siticoneTextBox1.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.siticoneTextBox1.HoverState.Parent = this.siticoneTextBox1;
+            this.siticoneTextBox1.Location = new System.Drawing.Point(12, 60);
+            this.siticoneTextBox1.Name = "siticoneTextBox1";
+            this.siticoneTextBox1.PasswordChar = '\0';
+            this.siticoneTextBox1.PlaceholderText = "";
+            this.siticoneTextBox1.SelectedText = "";
+            this.siticoneTextBox1.ShadowDecoration.Parent = this.siticoneTextBox1;
+            this.siticoneTextBox1.Size = new System.Drawing.Size(209, 26);
+            this.siticoneTextBox1.Style = Siticone.Desktop.UI.WinForms.Enums.TextBoxStyle.Material;
+            this.siticoneTextBox1.TabIndex = 1;
+            this.siticoneTextBox1.TextChanged += new System.EventHandler(this.siticoneTextBox1_TextChanged);
+            // 
+            // button
+            // 
+            this.button.BorderRadius = 12;
+            this.button.CheckedState.Parent = this.button;
+            this.button.CustomImages.Parent = this.button;
+            this.button.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.button.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.button.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.button.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.button.DisabledState.Parent = this.button;
+            this.button.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(50)))), ((int)(((byte)(50)))));
+            this.button.Font = new System.Drawing.Font("Arial", 8F);
+            this.button.ForeColor = System.Drawing.Color.White;
+            this.button.HoverState.Parent = this.button;
+            this.button.Location = new System.Drawing.Point(227, 60);
+            this.button.Name = "button";
+            this.button.ShadowDecoration.Parent = this.button;
+            this.button.Size = new System.Drawing.Size(105, 26);
+            this.button.TabIndex = 2;
+            this.button.Text = "Choose DLL";
+            this.button.Click += new System.EventHandler(this.button_Click);
+            // 
+            // siticoneRoundedComboBox1
+            // 
+            this.siticoneRoundedComboBox1.BackColor = System.Drawing.Color.Transparent;
+            this.siticoneRoundedComboBox1.BorderRadius = 17;
+            this.siticoneRoundedComboBox1.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawFixed;
+            this.siticoneRoundedComboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.siticoneRoundedComboBox1.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(50)))), ((int)(((byte)(50)))));
+            this.siticoneRoundedComboBox1.FocusedColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.siticoneRoundedComboBox1.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.siticoneRoundedComboBox1.FocusedState.Parent = this.siticoneRoundedComboBox1;
+            this.siticoneRoundedComboBox1.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.siticoneRoundedComboBox1.ForeColor = System.Drawing.Color.White;
+            this.siticoneRoundedComboBox1.HoverState.Parent = this.siticoneRoundedComboBox1;
+            this.siticoneRoundedComboBox1.ItemHeight = 30;
+            this.siticoneRoundedComboBox1.ItemsAppearance.Parent = this.siticoneRoundedComboBox1;
+            this.siticoneRoundedComboBox1.Location = new System.Drawing.Point(12, 115);
+            this.siticoneRoundedComboBox1.Name = "siticoneRoundedComboBox1";
+            this.siticoneRoundedComboBox1.ShadowDecoration.Parent = this.siticoneRoundedComboBox1;
+            this.siticoneRoundedComboBox1.Size = new System.Drawing.Size(235, 36);
+            this.siticoneRoundedComboBox1.TabIndex = 3;
+            // 
+            // button2
+            // 
+            this.button2.BorderRadius = 23;
+            this.button2.CheckedState.Parent = this.button2;
+            this.button2.CustomImages.Parent = this.button2;
+            this.button2.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.button2.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.button2.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.button2.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.button2.DisabledState.Parent = this.button2;
+            this.button2.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(50)))), ((int)(((byte)(50)))));
+            this.button2.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.White;
+            this.button2.HoverState.Parent = this.button2;
+            this.button2.Location = new System.Drawing.Point(80, 186);
+            this.button2.Name = "button2";
+            this.button2.ShadowDecoration.Parent = this.button2;
+            this.button2.Size = new System.Drawing.Size(188, 49);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Inject!";
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // siticoneRoundedButton1
+            // 
+            this.siticoneRoundedButton1.BorderRadius = 17;
+            this.siticoneRoundedButton1.CheckedState.Parent = this.siticoneRoundedButton1;
+            this.siticoneRoundedButton1.CustomImages.Parent = this.siticoneRoundedButton1;
+            this.siticoneRoundedButton1.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.siticoneRoundedButton1.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.siticoneRoundedButton1.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.siticoneRoundedButton1.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.siticoneRoundedButton1.DisabledState.Parent = this.siticoneRoundedButton1;
+            this.siticoneRoundedButton1.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(50)))), ((int)(((byte)(50)))));
+            this.siticoneRoundedButton1.Font = new System.Drawing.Font("Arial", 8F);
+            this.siticoneRoundedButton1.ForeColor = System.Drawing.Color.White;
+            this.siticoneRoundedButton1.HoverState.Parent = this.siticoneRoundedButton1;
+            this.siticoneRoundedButton1.Location = new System.Drawing.Point(253, 115);
+            this.siticoneRoundedButton1.Name = "siticoneRoundedButton1";
+            this.siticoneRoundedButton1.ShadowDecoration.Parent = this.siticoneRoundedButton1;
+            this.siticoneRoundedButton1.Size = new System.Drawing.Size(79, 36);
+            this.siticoneRoundedButton1.TabIndex = 5;
+            this.siticoneRoundedButton1.Text = "Reload processes";
+            this.siticoneRoundedButton1.Click += new System.EventHandler(this.siticoneRoundedButton1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(-1, 252);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(104, 18);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Status: Offline";
+            // 
+            // Injector
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(10)))), ((int)(((byte)(20)))), ((int)(((byte)(20)))));
+            this.ClientSize = new System.Drawing.Size(344, 279);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.siticoneRoundedButton1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.siticoneRoundedComboBox1);
+            this.Controls.Add(this.button);
+            this.Controls.Add(this.siticoneTextBox1);
+            this.Controls.Add(this.Panel);
+            this.ForeColor = System.Drawing.SystemColors.Control;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximizeBox = false;
+            this.Name = "Injector";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "DLL Injector";
+            this.Panel.ResumeLayout(false);
+            this.Panel.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Siticone.Desktop.UI.WinForms.SiticonePanel Panel;
+        private Siticone.Desktop.UI.WinForms.SiticoneTextBox siticoneTextBox1;
+        private Siticone.Desktop.UI.WinForms.SiticoneRoundedButton button;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private Siticone.Desktop.UI.WinForms.SiticoneRoundedComboBox siticoneRoundedComboBox1;
+        private Siticone.Desktop.UI.WinForms.SiticoneRoundedButton button2;
+        private Siticone.Desktop.UI.WinForms.SiticoneRoundedButton siticoneRoundedButton1;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Injector/Form1.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Injector
+{
+    public partial class Injector : Form
+    {
+        protected override void WndProc(ref Message m)
+        {
+            base.WndProc(ref m);
+            if (m.Msg == WM_NCHITTEST)
+                m.Result = (IntPtr)(HT_CAPTION);
+        }
+        private const int WM_NCHITTEST = 0x84;
+        private const int HT_CAPTION = 0x2;
+
+        public Injector()
+        {
+            InitializeComponent();
+
+            Process[] PC = Process.GetProcesses().Where(p => (long)p.MainWindowHandle != 0).ToArray();
+            siticoneRoundedComboBox1.Items.Clear();
+            foreach (Process p in PC)
+            {
+                siticoneRoundedComboBox1.Items.Add(p.ProcessName);
+            }
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(0);
+        }
+        private static string DLLP { get; set; }
+        private void button_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                OpenFileDialog ofd = new OpenFileDialog();
+                ofd.InitialDirectory = @"C:\";
+                ofd.Title = "Locate DLL";
+                ofd.DefaultExt = "dll";
+                ofd.Filter = "DLL Files (*.dll)|*.dll";
+                ofd.CheckFileExists = true;
+                ofd.CheckPathExists = true;
+                ofd.ShowDialog();
+
+                siticoneTextBox1.Text = ofd.FileName;
+
+                DLLP = ofd.FileName;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        private void siticoneTextBox1_TextChanged(object sender, EventArgs e)
+        {
+            DLLP = siticoneTextBox1.Text;
+        }
+        private void siticoneRoundedButton1_Click(object sender, EventArgs e)
+        {
+            Process[] PC = Process.GetProcesses().Where(p => (long)p.MainWindowHandle != 0).ToArray();
+            siticoneRoundedComboBox1.Items.Clear();
+            foreach (Process p in PC)
+            {
+                siticoneRoundedComboBox1.Items.Add(p.ProcessName);
+            }
+        }
+
+        /*
+         * Injection Method
+         */
+        static readonly IntPtr INTPTR_ZERO = (IntPtr)0;
+        [DllImport("kernel32.dll", SetLastError = true)]
+
+        static extern IntPtr OpenProcess(uint dwDesiredAccess, int bInheritHandle, uint dwProcessId);
+        [DllImport("kernel32.dll", SetLastError = true)]
+
+        static extern int CloseHandle(IntPtr intPtr);
+        [DllImport("kernel32.dll", SetLastError = true)]
+
+        static extern IntPtr GetProcAddress(IntPtr hModule, string lpProcName);
+        [DllImport("kernel32.dll", SetLastError = true)]
+
+        static extern IntPtr GetModuleHandle(string lpModuleName);
+        [DllImport("kernel32.dll", SetLastError = true)]
+
+        static extern IntPtr VirtualAllocEx(IntPtr hProcess, IntPtr lpAddress, IntPtr dwSize, uint flAllocationType, uint flProtect);
+        [DllImport("kernel32.dll", SetLastError = true)]
+
+        static extern IntPtr WriteProcessMemory(IntPtr hProcess, IntPtr lpBaseAddress, byte[] buffer, uint size, int lpNumberOfBytesWritten);
+        [DllImport("kernel32.dll", SetLastError = true)]
+        static extern IntPtr CreateRemoteThread(IntPtr hProcess, IntPtr lpThreadAttribute, IntPtr dwStackSize, IntPtr lpStartAddress, IntPtr lpParameter, uint dwCreationFlags, IntPtr lpThreadId);
+
+        public static bool SI(uint P, string DLLP)
+        {
+            IntPtr hndProc = OpenProcess((0x2 | 0x8 | 0x10 | 0x20 | 0x400), 1, P);
+            if (hndProc == IntPtr.Zero) { return false; }
+
+            IntPtr lpAddress = VirtualAllocEx(hndProc, (IntPtr)null, (IntPtr)DLLP.Length, (0x1000 | 0x2000), 0x40);
+            if (lpAddress == IntPtr.Zero) { return false; }
+
+            byte[] bytes = Encoding.ASCII.GetBytes(DLLP);
+
+            if (WriteProcessMemory(hndProc, lpAddress, bytes, (uint)bytes.Length, 0).Equals(0))
+            { 
+                return false; 
+            }
+
+            CloseHandle(hndProc);
+            return true;
+        }
+
+        public static int Inject(string PN, string DLLP)
+        {
+            // 1 = File doesn't exist
+            // 2 = doesn't exist
+            if (!File.Exists(DLLP)) { return 1; }
+
+            uint _procId = 0;
+            Process[] _procs = Process.GetProcesses();
+            for (int i = 0; i < _procs.Length; i++)
+            {
+                if(_procs[i].ProcessName == PN)
+                {
+                    _procId = (uint)_procs[i].Id;
+                }
+            }
+
+            if(_procId == 0) { return 2; }
+            if(!SI(_procId, DLLP))
+            {
+                return 3;
+            }
+
+            return 4;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int Result = Inject(siticoneRoundedComboBox1.Text, DLLP);
+
+            if(Result == 1)
+            {
+                MessageBox.Show("The file doesn't exist..!");
+                label3.Text = "Status: Offline!";
+            }
+            else if(Result == 2)
+            {
+                MessageBox.Show("The process doesn't exist..!");
+                label3.Text = "Status: Offline!";
+            }
+            else if(Result == 3)
+            {
+                MessageBox.Show("The injection failed..!");
+                label3.Text = "Status: Injection failed!";
+            }
+            else if(Result == 4)
+            {
+                MessageBox.Show("Injection succeded!");
+                label3.Text = "Status: Injection succeded!";
+            }
+        }
+
+        private void Panel_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
